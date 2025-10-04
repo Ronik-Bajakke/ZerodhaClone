24b55a3dd0216c73a33c8b85882a88c6d95826ce
Ronik-Bajakke <ronikbajakke@172913@gmail.com>
2025-10-04 20:07:22 +0530

    done

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -11,7 +11,7 @@ function Navbar() {
     }
   }, []);
 
-  const FRONTEND_URL = process.env.REACT_APP_FRONTEND_URL || "http://localhost:3000";
+  const FRONTEND_URL = process.env.REACT_APP_FRONTEND_URL;
 
   const handleLogout = () => {
     localStorage.removeItem("token");
